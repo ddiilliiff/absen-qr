9b13833019e5215ef62691a1c748069f0170e064
DILIF-PROJECT <ddiilliiff@hotmail.com>
2023-11-25 21:51:05 +0900

    Kontroller dosen

--- app/Controllers/Dosen.php
+++ app/Controllers/Dosen.php
@@ -2,13 +2,28 @@
 
 namespace App\Controllers;
 
+use App\Models\AbsenModel;
 use App\Models\DosenModel;
+use App\Models\JadwalModel;
+use App\Models\MahasiswaModel;
+use App\Models\MatkulModel;
 
 class Dosen extends BaseController
 {
     public function __construct()
     {
-        
+        $this->absen = new AbsenModel();
+        $this->dosen = new DosenModel();
+        $this->jadwal = new JadwalModel();
+        $this->mahasiswa = new MahasiswaModel();
+        $this->matkul = new MatkulModel();
     }
 
+    public function index()
+    {
+        $dosenModel = new DosenModel();
+        $dosen = $dosenModel->dosen();
+
+        return view('dosen.index', compact('dosen'));
+    }
 }
